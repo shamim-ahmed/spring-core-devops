91e2747a72e35d536d25d800dcdd7d75ce0e5245
Shamim Ahmed <shamim.buet.99@gmail.com>
2022-04-03 01:02:08 +1100

    Demonstrate how to configure multiple properties files

--- src/test/java/guru/test/config/external/props/ExternalPropsEnvironment.java
+++ src/test/java/guru/test/config/external/props/ExternalPropsEnvironment.java
@@ -5,25 +5,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.core.env.Environment;
 
 /**
  * Created by jt on 5/7/16.
  */
 @Configuration
-@PropertySource("classpath:testing.properties")
+@PropertySources({ @PropertySource("classpath:testing.properties"),
+        @PropertySource("classpath:encrypted-testing.properties") })
 public class ExternalPropsEnvironment {
 
     @Autowired
-    Environment env; //also from Spring 3.1
+    Environment env; // also from Spring 3.1
 
     @Bean
-    public FakeJmsBroker fakeJmsBrokerEnv(){
+    public FakeJmsBroker fakeJmsBrokerEnv() {
         FakeJmsBroker fakeJmsBroker = new FakeJmsBroker();
         fakeJmsBroker.setUrl(env.getProperty("guru.jms.server"));
         fakeJmsBroker.setPort(env.getRequiredProperty("guru.jms.port", Integer.class));
         fakeJmsBroker.setUser(env.getProperty("guru.jms.user"));
-        fakeJmsBroker.setPassword(env.getProperty("guru.jms.password"));
+        fakeJmsBroker.setPassword(env.getProperty("guru.jms.encrypted.password"));
         return fakeJmsBroker;
     }
 }
